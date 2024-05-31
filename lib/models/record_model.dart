e1496f1568270b22e5d067eccb82f4caa5eb1a66
Jaime Andrade <jaimeandrade454@gmail.com>
2024-05-31 12:06:22 -0500

    Use optional values in Record model

--- lib/models/record_model.dart
+++ lib/models/record_model.dart
@@ -1,29 +1,30 @@
 class Record {
-  final int id;
-  final DateTime createdAt;
-  final String? doctorId;
-  final String clientId;
-  final int systolic;
-  final int diastolic;
-  final int bpm;
-  final int? weight;
-  final int? spO2;
+  int? id;
+  DateTime createdAt;
+  String? doctorId;
+  String? clientId;
+  int? systolic;
+  int? diastolic;
+  int? bpm;
+  int? weight;
+  int? spO2;
 
-  Record(
-      {required this.id,
-      required this.createdAt,
-      this.doctorId,
-      required this.clientId,
-      required this.systolic,
-      required this.diastolic,
-      required this.bpm,
-      this.weight,
-      this.spO2});
+  Record({
+    this.id,
+    DateTime? createdAt_,
+    this.doctorId,
+    this.clientId,
+    this.systolic,
+    this.diastolic,
+    this.bpm,
+    this.weight,
+    this.spO2,
+  }) : createdAt = createdAt_ ?? DateTime.now();
 
   factory Record.fromJson(Map<String, dynamic> json) {
     return Record(
         id: json['id'],
-        createdAt: DateTime.parse(json['created_at'] as String),
+        createdAt_: DateTime.parse(json['created_at'] as String),
         doctorId: json['doctor_id'],
         clientId: json['client_id'],
         systolic: json['systolic'],
