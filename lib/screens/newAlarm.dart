7839d351cd26bff8bbcffec453b5beefb0abee3a
Jaime Andrade <jaimeandrade454@gmail.com>
2024-06-12 11:33:10 -0500

    New alarm screen

---
+++ lib/screens/newAlarm.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:heart/widgets/widgets.dart';
+
+class NewAlarmScreen extends StatefulWidget {
+  const NewAlarmScreen({super.key});
+
+  @override
+  State<NewAlarmScreen> createState() => _NewAlarmScreenState();
+}
+
+class _NewAlarmScreenState extends State<NewAlarmScreen> {
+  late final TextEditingController _title;
+  late TimeOfDay _selectedTime;
+
+  @override
+  void initState() {
+    _title = TextEditingController();
+    _selectedTime = TimeOfDay.now();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _title.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black26),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(children: [
+          Text('New alarm', style: TextStyle(color: Colors.black54)),
+        ]),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(
+              'Title:',
+            ),
+            TextField(
+              controller: _title,
+              decoration: InputDecoration(hintText: 'title'),
+            ),
+            TextButton(
+              child: Text("${_selectedTime.hour}:${_selectedTime.minute}"),
+              onPressed: () async {
+                final TimeOfDay? timeOfDay = await showTimePicker(
+                    context: context,
+                    initialTime: _selectedTime,
+                    initialEntryMode: TimePickerEntryMode.dial);
+                if (timeOfDay != null) {
+                  setState(() {
+                    _selectedTime = timeOfDay;
+                  });
+                }
+              },
+            ),
+            SaveCanceltToolbarWidget(
+                onPressedCallback: () => {},
+                onCancelCallback: () => {Navigator.pop(context)})
+          ],
+        ),
+      ),
+    );
+  }
+}
